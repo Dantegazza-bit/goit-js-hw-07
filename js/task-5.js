f0847adf57068e0a7c1d63f114442d016d3be2b3
Oleksandr <oleksandrzabolotny@gmail.com>
2025-08-22 15:24:05 +0200

    HW-07: tasks 1-5 completed

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// Отримуємо елементи з HTML
+const btn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+// Додаємо слухач на кнопку
+btn.addEventListener("click", () => {
+  // Генеруємо новий колір
+  const color = getRandomHexColor();
+
+  // Змінюємо фон сторінки
+  document.body.style.backgroundColor = color;
+
+  // Виводимо колір у span
+  colorSpan.textContent = color;
+});
